78f2399b0328231b98980ceae84434c511e2d668
owais4321 <owais4321>
2020-04-29 05:53:33 -0700

    added second class

---
+++ class 2/code/class2.py
@@ -0,0 +1,72 @@
+# arr=[1,2,3,'a','ehllo',1+2j]
+# print(arr)
+# tup=(1,2,3,'1',2+3j)
+# print(tup)
+# dictionary={'one':1,'two':2,3+3j:3}
+# print(dictionary)
+# a=10
+# b=2
+# add=a+b
+# sub=a-b
+# mul=a*b
+# div=a/b
+# rem=a%b
+# exp=a**b
+# flrdiv=a//b
+# print(add)
+# print(sub)
+# print(mul)
+# print(div)
+# print(rem)
+# print(exp)
+# print(flrdiv)
+# str="hello world"
+# print(str[1])
+# print(str[1:5])
+# print(str[:5])
+# print(str[2:])
+# str=str[1:4]
+# print(str)
+# str1="#"
+# str2="*"
+# str3=str1+str2
+# print(str3\r)
+# print(str2*10)
+# print(str1 in str3)
+# print(str1 not in str3)
+# print("a" not in str)
+arr=[1,2,3,4,5,6,7]
+print(arr[2])
+print(arr[:2])
+print(arr[1:])
+print(arr[1:3])
+print(arr*3)
+print(arr+arr)
+print(1 in arr)
+print(10 in arr)
+print(2 not in arr)
+print(10 not in arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class 2/code/test.py
@@ -0,0 +1,77 @@
+# list_1=[1,1.1,1+2j,'hello world']
+# print(list_1)
+# list_2=[1,1.1,1+2j,'hello world',[1,2,3,4,5]]
+# print(list_2)
+# list_nested=[[1,2],[1,2]]
+# print(list_nested)
+# tup_1=(1,2,4,'hello world')
+# print(tup_1)
+# tup_2=(1,1.1,1+2j,'hello world',[1,2,3,4,5])
+# print(tup_2)
+# tup_nest=((1,2),(3,4))
+# print(tup_nest)
+# tup_nest2=((1,2),((1,2),3,4))
+# print(tup_nest2)
+# dictionary={'key':123456}
+# print(dictionary)
+# dictionary_1={'int':1,'float':1.2,'comp':1+2j,"str":"1234hello","list":[1,2,4],"tuple":(1,2,3),'dict':dictionary}
+# print(dictionary_1)
+# dictionary_3={"dict1":dictionary,"dict2":dictionary_1}
+# print(dictionary_3)
+# user={
+#     'name':'owais',
+#     'email':'example@example.com',
+#     'password':'xyz',
+#     'addresss':{
+#         'street':2,
+#         'city':'karachi'
+#     }
+# }
+# print(user)
+#perform operation on numbers
+# a=4
+# b=3
+# sum=(a+b)
+# sub=(a-b)
+# mul=(a*b)
+# div=(a/b)
+# print(sum)
+# print(sub)
+# print(mul)
+# print(div)
+# rem=a%b
+# print(rem)
+# exp=a**b
+# print(exp)
+# flrdiv=a//b
+# print(flrdiv)
+str1='hello world'
+w=str1[6]
+seq=str1[0:6]
+seq1=str1[:6]
+print(w)
+print(seq)
+print(seq1)
+str1=str1[3:]
+print(str1)
+str4="hi"
+str5=",how are you"
+str6=str4+str5
+print(str6)
+str7=str4*5
+print(str7)
+print("hola" in str6)
+print("hola" not in str6)
+print("hi" not in str6)
+
+
+
+
+
+
+
+
+
+
+
+
